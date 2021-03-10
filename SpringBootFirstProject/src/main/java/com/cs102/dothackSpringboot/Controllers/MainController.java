daf931b46f584fff71c257c9d0b5a7d944abc94a
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-03-10 12:53:22 +0800

    Commit

---
+++ SpringBootFirstProject/src/main/java/com/cs102/dothackSpringboot/Controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.cs102.dothackSpringboot.Controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping(path = "demo/test1")
+    public String test1() {
+        return "Welcome to dot hack workshop";
+    }
+}
--- dothackSpringboot/src/main/java/Controllers/MainController.java
+++
@@ -1,9 +0,0 @@
-package Controllers;
-
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class MainController {
-    
-}
