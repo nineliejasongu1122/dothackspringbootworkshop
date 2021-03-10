d8ab44884603b45ee510968190764ce319ae65a7
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-03-10 14:29:50 +0800

    Update

--- SpringBootFirstProject/src/main/java/com/cs102/dothackSpringboot/Controllers/MainController.java
+++ SpringBootFirstProject/src/main/java/com/cs102/dothackSpringboot/Controllers/MainController.java
@@ -1,13 +1,30 @@
 package com.cs102.dothackSpringboot.Controllers;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-    @GetMapping(path = "demo/test1")
-    public String test1() {
-        return "Welcome to dot hack workshop";
+    @GetMapping(path = "/get/test1")
+    public String getWithNothing(){
+        return "WELCOME TO .hack SPRING BOOT WORKSHOP";
+    }
+
+    @GetMapping(path = "/get/test2/{text}")
+    public String getWithPathVar(@PathVariable String text){
+        return "The Text is: " + text;
+    }
+
+    @GetMapping(path = "/get/test3")
+    public String getWithRequestParam(@RequestParam String text){
+        return "The Text is: " + text;
+    }
+
+
+    @GetMapping(path = "/get/test4")
+    public String getWithDefaultParam(@RequestParam (defaultValue = "I always love java") String text){
+        return "The Text is: " + text;
     }
 }
