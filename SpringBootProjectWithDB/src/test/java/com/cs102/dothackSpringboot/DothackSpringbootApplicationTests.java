64b89d47475ab97088281b8d2a31ea5119d68181
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-03-10 12:54:34 +0800

    Commit

---
+++ SpringBootProjectWithDB/src/test/java/com/cs102/dothackSpringboot/DothackSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs102.dothackSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DothackSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
