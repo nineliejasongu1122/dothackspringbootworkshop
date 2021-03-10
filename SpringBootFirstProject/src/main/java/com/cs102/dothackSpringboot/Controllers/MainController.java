6e142dc66e80bdb0ce03fa54ca725495a686b9a8
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-03-10 15:08:13 +0800

    jpa

--- SpringBootFirstProject/src/main/java/com/cs102/dothackSpringboot/Controllers/MainController.java
+++ SpringBootFirstProject/src/main/java/com/cs102/dothackSpringboot/Controllers/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class MainController {
     @GetMapping(path = "demo/test1")
     public String test1() {
---
+++ SpringBootFirstProject/src/main/java/com/cs102/dothackSpringboot/Entities/Student.java
@@ -0,0 +1,12 @@
+package com.cs102.dothackSpringboot.Entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+}
