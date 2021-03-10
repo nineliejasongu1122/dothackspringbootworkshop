d81551bfbe5cb517747af3ed9d55187a0b9e74ee
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-03-10 12:40:28 +0800

    First Commit

---
+++ dothackSpringboot/src/main/java/com/cs102/dothackSpringboot/DothackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.cs102.dothackSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DothackSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DothackSpringbootApplication.class, args);
+	}
+
+}
