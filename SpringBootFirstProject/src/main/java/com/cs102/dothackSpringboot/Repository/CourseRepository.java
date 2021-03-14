490e8f9081dcfc5798323d3e26023fd9af433a32
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-03-14 16:15:24 +0800

    Update Description

--- SpringBootFirstProject/src/main/java/com/cs102/dothackSpringboot/Repository/CourseRepository.java
+++ SpringBootFirstProject/src/main/java/com/cs102/dothackSpringboot/Repository/CourseRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
 public interface CourseRepository extends CrudRepository<Course, Integer> {
 
 
