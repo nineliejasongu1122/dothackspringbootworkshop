ebae506230dec1be8b045b7b7da6f4d8081ea739
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-03-17 21:55:03 +0800

    update naming

--- SpringBootFirstProject/src/main/java/com/cs102/dothackSpringboot/controllers/MainController.java
+++ SpringBootFirstProject/src/main/java/com/cs102/dothackSpringboot/controllers/MainController.java
@@ -1,6 +1,5 @@
-package com.cs102.dothackSpringboot.Controllers;
+package com.cs102.dothackSpringboot.controllers;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
