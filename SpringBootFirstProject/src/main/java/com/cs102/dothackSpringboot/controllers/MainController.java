abeab657b6d27e3632a5bd230cf680fea6d2179e
xbowery <69230356+xbowery@users.noreply.github.com>
2023-03-03 18:09:59 +0800

    Add in OOP concepts for demo display

--- SpringBootFirstProject/src/main/java/com/cs102/dothackSpringboot/controllers/MainController.java
+++ SpringBootFirstProject/src/main/java/com/cs102/dothackSpringboot/controllers/MainController.java
@@ -2,12 +2,28 @@ package com.cs102.dothackSpringboot.controllers;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cs102.dothackSpringboot.classes.Counter;
 
 @RestController
 public class MainController {
+
+    Counter counter = new Counter();
+
+    @PostMapping(path = "demo/pray")
+    public void pray() {
+        counter.increment();
+    }
+
     @GetMapping(path = "demo/test1")
     public String test1() {
         return "Welcome to dot hack workshop";
+    }
+
+    @GetMapping(path = "demo/display")
+    public String display() {
+        return "Total prayers: " + counter.getCount();
     }
 }
